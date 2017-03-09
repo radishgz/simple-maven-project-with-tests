120a938a409aefb8afa644986b7042a88f004581
radishgz <radishgz@gmail.com>
2017-03-09 17:48:54 +0800

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,9 +31,9 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.2) {
+        if (r < 0.1) {
             fail("oops");
-        } else if (r < 0.3) {
+        } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
         }
     }
