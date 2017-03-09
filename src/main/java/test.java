61555fe256a55983e217c3a0cdceab8482e1e460
radishgz <radishgz@gmail.com>
2017-03-09 18:07:44 +0800

    Create test.java

---
+++ src/main/java/test.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
+import static lambdasinaction.chap4.Dish.menu;
+
+public class StreamBasic {
+
+    public static void main(String...args){
+    int i=10;
+    int j=20;
+    }
+    }
+    
