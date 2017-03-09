dfa277c4fdb5a0124e6d06c90c5fb5f597b12646
radishgz <radishgz@gmail.com>
2017-03-09 18:08:51 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,12 +1,7 @@
 import java.util.*;
-import java.util.stream.Stream;
+ 
 
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
-
-import static lambdasinaction.chap4.Dish.menu;
-
-public class StreamBasic {
+public class test {
 
     public static void main(String...args){
     int i=10;
